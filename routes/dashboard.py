0c19b8af3a8bf43cf64155ee1d781767f5338b9f
maria lucato <mariaelisbrolezzi4@gmail.com>
2026-03-18 11:21:17 -0300

    variacao de nomes e email na page dashboard feita

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request
+from database.models.users import User_info 
 
 dashboard_route = Blueprint('dashboard_route', __name__)
 
 @dashboard_route.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html') # função que retorna algo quando a rota /dashboard for acessada #
+    id = request.args.get('id')
+    gettingUser = User_info.get_or_none(User_info.id == id)
+    
+    if gettingUser == None:
+        return 'Usuário não encontrado'
+    else:
+        return render_template('dashboard.html', name=gettingUser.name, email=gettingUser.email) 
