111b65e47ce0a9fe105447f2ea68951593e0f41b
maria lucato <mariaelisbrolezzi4@gmail.com>
2026-03-19 14:04:03 -0300

    mudanca na cor do cards + modal de add card

--- database/models/cards.py
+++ database/models/cards.py
@@ -3,10 +3,10 @@ from database.database import db
 from database.models.users import User_info
 
 class Card_info(Model): # Define a classe Card_info que herda de Model, representando a tabela de cartões no banco de dados #
-    titularEmail = ForeignKeyField(User_info, backref='email') # Define uma chave estrangeira para a tabela de usuários, relacionando os cartões aos seus respectivos usuários #
+    titularEmail = ForeignKeyField(User_info, backref='cards') # Define uma chave estrangeira para a tabela de usuários, relacionando os cartões aos seus respectivos usuários #
     titularName = CharField()
-    numerCard = CharField(unique=True)
-    expiradion = CharField(4)
+    numberCard = CharField(unique=True)
+    expiration = CharField(4)
     cvv = CharField(3)
     
     class Meta: # Define a classe Meta para configurar o modelo #
--- database/models/users.py
+++ database/models/users.py
@@ -1,11 +1,10 @@
-from peewee import Model, CharField
+from peewee import IntegerField, Model, CharField
 from database.database import db
 
 class User_info(Model): # Define a classe User_info que herda de Model, representando a tabela de usuários no banco de dados #
     name = CharField()
     email = CharField(unique=True)
     password = CharField()
-    creditsCards = CharField(default=0) # quantidade de cartões cadastrados pelo usuário, iniciando com 0 por padrão # 
     
     class Meta: # Define a classe Meta para configurar o modelo #
         database = db
