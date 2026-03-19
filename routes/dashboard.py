8af4d7bcd0a503359f2236386dda91d78dc6d824
maria lucato <mariaelisbrolezzi4@gmail.com>
2026-03-19 14:42:59 -0300

    form de de cartao ok

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
+from database.models.cards import Card_info
 from database.models.users import User_info 
 
 dashboard_route = Blueprint('dashboard_route', __name__)
@@ -12,3 +13,15 @@ def dashboard():
         return 'Usuário não encontrado'
     else:
         return render_template('dashboard.html', name=gettingUser.name, email=gettingUser.email) 
+    
+@dashboard_route.route('/dashboard', methods=['POST'])
+def dashboard_new_card():
+    data = request.form # dados do formulário enviado #
+    Card_info.create(
+        holderEmail = 'admin@gmail.com',
+        numberCard = data['cardNumber'],
+        holderName = data['holderName'].upper(),
+        expiration = data['expiration'],
+        cvv = data['cvv']
+    )
+    return redirect(url_for('dashboard_route.dashboard'))
